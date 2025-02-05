27eb0b3edae7dab3013fac854c83b5898397c656
yasirzain15 <softwareguy1515@gmail.com>
2025-02-05 20:43:24 +0500

    done

--- lib/multiple_entries.dart
+++ lib/multiple_entries.dart
@@ -8,12 +8,6 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MaterialApp(home: CsvExcelUploader()));
-}
-
 class CsvExcelUploader extends StatefulWidget {
   const CsvExcelUploader({super.key});
 
